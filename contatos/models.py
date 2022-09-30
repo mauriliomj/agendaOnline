8a1fb69aa616eb00768a96d49c01e11f633d0f3c
maurilio <mauriliojose249@gmail.com>
2022-09-30 15:34:18 -0300

    Initial commit

---
+++ contatos/models.py
@@ -0,0 +1,41 @@
+from django.db import models
+from django.utils import timezone
+
+'''
+
+CONTATOS
+nome: STR * (obrigatório)
+sobrenome: STR (opcional)
+telefone: STR * (obrigatório)
+email: STR (opcional)
+data_criacao: DATETIME (automático)
+descricao: texto
+categoria: CATEGORIA (outro model)
+
+CATEGORIA
+nome: STR * (obrigatório)
+
+'''
+
+
+class Categoria(models.Model):
+    nome = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.nome
+
+
+class Contato(models.Model):
+    nome = models.CharField(max_length=150)
+    sobrenome = models.CharField(max_length=255, blank=True)
+    telefone = models.CharField(max_length=255)
+    email = models.CharField(max_length=255, blank=True)
+    data_criacao = models.DateTimeField(default=timezone.now)
+    descricao = models.TextField(blank=True)
+    categoria = models.ForeignKey(Categoria, on_delete=models.DO_NOTHING)
+    mostrar = models.BooleanField(default=True)
+    foto = models.ImageField(blank=True, upload_to='fotos/%Y/%m/')
+
+    def __str__(self):
+        return self.nome
+
